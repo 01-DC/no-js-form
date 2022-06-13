8c3b0e31c7c8ad637619968b8f16465b976f9150
Devanshu Chatterjee <devanshu4190@gmail.com>
2022-06-13 12:18:07 +0530

    form validation logic using js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 // write your JavaScript here
 
-const form = document.forms["form1"]
+const form = document.forms.Form1
+
+form.Name.addEventListener('input', (e) =>
+{
+    nameSpan = document.querySelector('#name-status')
+    val = e.target.value.trim()
+    if (val === '' || val == null)
+        nameSpan.textContent = "🔴"
+    else
+        nameSpan.textContent = "🟢"
+})
+
+form.Email.addEventListener('input', (e) =>
+{
+    emailSpan = document.querySelector('#email-status')
+    val = e.target.value.trim()
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(val))
+        emailSpan.textContent = "🟢"
+    else
+        emailSpan.textContent = "🔴"
+})
